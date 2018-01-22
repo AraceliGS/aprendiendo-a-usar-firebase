a184bc9803ead2ccb4c8ce30a9681d22c978eba1
AraceliGS <aragutarras@gmail.com>
2018-01-22 16:56:48 -0500

    añadiendo plantilla base ya enlazado a firebase

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCR-3g5Nd4LWmti1EpV8bp7JOwJQ8Ps_Kg",
+  authDomain: "mi-proyecto-3b344.firebaseapp.com",
+  databaseURL: "https://mi-proyecto-3b344.firebaseio.com",
+  projectId: "mi-proyecto-3b344",
+  storageBucket: "",
+  messagingSenderId: "978784732774"
+};
+firebase.initializeApp(config);
