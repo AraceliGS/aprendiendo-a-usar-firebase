a336d2e6482b8221febdc0b3968a285886fe912c
AraceliGS <aragutarras@gmail.com>
2018-01-23 16:53:34 -0500

    añadiendo ligeras modificaciones

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
     firebase.auth().signInWithPopup(provider).then(function(result) {
       // This gives you a Google Access Token. You can use it to access the Google API.
       var token = result.credential.accessToken;
-      // The signed-in user info.
+      // The signed-in user info.F
       var user = result.user;
       // ...
     });
